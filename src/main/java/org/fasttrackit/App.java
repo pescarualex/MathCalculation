516e2d6f4a30530082d9f6d8a3f373276a6da09b
Pescaru Alexandru <pescarualexflorin@gmail.com>
2020-06-07 13:45:48 +0300

    added some methods with calculation

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -1,13 +1,22 @@
 package org.fasttrackit;
 
-/**
- * Hello world!
- *
- */
+
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) {
+        MethodsClass math = new MethodsClass();
+        System.out.println(math.sumOfTwoNum(5,2));
+
+        System.out.println(math.difecence(10,5));
+
+        System.out.println(math.zecimalCalculation(15.2, 4.3));
+
+        System.out.println(math.multiplication(5,4));
+
+        System.out.println(math.multiplication2(5.8, 7.3));
+
+        System.out.println(math.division(65,48));
+
+
     }
 }
---
+++ src/main/java/org/fasttrackit/MethodsClass.java
@@ -0,0 +1,27 @@
+package org.fasttrackit;
+
+public class MethodsClass {
+    public int sumOfTwoNum(int x, int y){
+        return x + y;
+    }
+
+    public int difecence(int a, int b){
+        return a - b;
+    }
+
+    public double zecimalCalculation ( double x, double y){
+        return x + y;
+    }
+
+    public int multiplication ( int x, int y){
+        return x * y;
+    }
+
+    public double multiplication2( double x, double y){
+        return x * y;
+    }
+
+    public double division( double x, double y){
+        return x / y;
+    }
+}
