1ef2e480d65fdadce51b164f99902450e19fec24
Pescaru Alexandru <pescarualexflorin@gmail.com>
2020-06-23 17:49:36 +0300

    added overloading methods

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -6,16 +6,23 @@ public class App
     public static void main( String[] args ) {
         MethodsClass math = new MethodsClass();
         System.out.println(math.sumOfTwoNum(5,2));
+        System.out.println(math.sumOfTwoNum(5));
+        System.out.println(" ");
 
         System.out.println(math.difecence(10,5));
+        System.out.println(math.difecence(10));
+        System.out.println(" ");
 
         System.out.println(math.zecimalCalculation(15.2, 4.3));
+        System.out.println(math.zecimalCalculation(15));
+        System.out.println(" ");
 
         System.out.println(math.multiplication(5,4));
-
-        System.out.println(math.multiplication2(5.8, 7.3));
+        System.out.println(math.multiplication(5));
+        System.out.println(" ");
 
         System.out.println(math.division(65,48));
+        System.out.println(math.division(65));
 
 
     }
--- src/main/java/org/fasttrackit/MethodsClass.java
+++ src/main/java/org/fasttrackit/MethodsClass.java
@@ -4,24 +4,42 @@ public class MethodsClass {
     public int sumOfTwoNum(int x, int y){
         return x + y;
     }
+    public double sumOfTwoNum(double x){
+        double y = 5;
+        return x + y;
+    }
 
     public int difecence(int a, int b){
         return a - b;
     }
+    public double difecence(double a){
+        double b = 5;
+        return a - b;
+    }
+
 
     public double zecimalCalculation ( double x, double y){
+        return x + y;
+    }
+    public int zecimalCalculation ( int x){
+        int y = 5;
         return x + y;
     }
 
     public int multiplication ( int x, int y){
         return x * y;
     }
-
-    public double multiplication2( double x, double y){
+    public double multiplication ( double x){
+        double y = 5;
         return x * y;
     }
 
+
     public double division( double x, double y){
+        return x / y;
+    }
+    public int division( int x){
+        int y = 5;
         return x / y;
     }
 }
